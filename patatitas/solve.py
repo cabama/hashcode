346edd073212b51dec53b5713c6d8e656351d92c
Butakus <butakus@gmail.com>
2016-02-11 19:54:25 +0100

    cacaca

--- patatitas/solve.py
+++ patatitas/solve.py
@@ -1,4 +1,7 @@
+from math import sqrt
 
+def euc(a,b):
+	return sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
 if __name__ == '__main__':
 	print 'Patatitas!'
@@ -29,3 +32,18 @@ if __name__ == '__main__':
 	print order_pos
 	print order_products
 
+	dist_order_warehouse = [0]*C
+	warehouse_order = [0]*W
+
+	for i in xrange(len(order_pos)):
+		dist_order_warehouse[i] = []
+		for j in xrange(len(warehouses)):
+			dist_order_warehouse[i] += [[euc(order_pos[i], warehouses[j]),j]]
+		dist_order_warehouse[i].sort()
+		if warehouse_order[dist_order_warehouse[i][0][1]] == 0:
+			warehouse_order[dist_order_warehouse[i][0][1]] = []
+		warehouse_order[dist_order_warehouse[i][0][1]] += [[order_products[i], i]]
+		print 'dist_irder_warehouse[i]: {}'.format(dist_order_warehouse[i])
+
+	for i in xrange(W):
+		print 'warehouse_order[i]: {}'.format(warehouse_order[i])
